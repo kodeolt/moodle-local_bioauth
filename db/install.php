4b248dae19ef74e21756c04b4937c6dfea3f595d
Vinnie Monaco <vinmonaco@gmail.com>
2013-06-25 00:33:50 -0400

    Added demo events is json files, started modifying tables to store keystrokes as json-encoded strings for better performance

--- db/install.php
+++ db/install.php
@@ -84,17 +84,24 @@ function load_demo_events() {
     global $CFG;
     global $DB;
     
-    //$userevents = new DefaultArray(new DefaultArray(new ArrayObject()));
-    $fields = array('userid', 'sessionid', 'keyid', 'timepress', 'timerelease');
-    if (($handle = fopen($CFG -> dirroot . '/local/bioauth/bootstrap/events.csv', "r")) !== FALSE) {
-        while (($csvdata = fgetcsv($handle, 1000, ",")) !== FALSE) {
-            if (5 === count($csvdata)) {
-                    $csvdata[2] = translate_keycode('native', 'en_US', $csvdata[2]);
-                    //$userevents[$csvdata[0]][$csvdata[1]][] = array_slice($csvdata, 2);
-                    $DB -> insert_record_raw('bioauth_demo_keystrokes', array_combine($fields, $csvdata), false, true);
-                }
+    $userit = new DirectoryIterator($CFG -> dirroot . '/local/bioauth/bootstrap');
+    
+    foreach ($localeit as $user) {
+        if ($locale->isDot()) continue;
+        $sessionit = new DirectoryIterator($user->getPathname());
+        foreach ($sessionit as $session) {
+            if ($agent->isDot()) continue;
+            
+            $jsonstring = file_get_contents($session->getPathname());
+            $keystrokes = json_decode($jsonstring, true);
+            
+            foreach ($keystrokes as $keystroke) {
+                $keystroke['keyid'] = translate_keycode('native', 'en_US', $keystroke['keycode']);
+                unset($keystroke['keycode']);
+            }
+            
+            $DB -> insert_record_raw('bioauth_demo_sessions', array('userid' => $user->getBasename(), 'locale' => 'en_US', 'keystrokes' => json_encode($keystrokes), 'stylometry' => ''), false, true);
         }
-        fclose($handle);
     }
 }
 
@@ -145,98 +152,98 @@ function xmldb_local_bioauth_install() {
     $keystrokefeatures = array(
     
     /* 
-     * Mean Durations 
+     * Mean Durations
      */
     1 => array(BIOAUTH_FEATURE_DURATION, $visiblekeys, $visiblekeys, BIOAUTH_MEASURE_MEAN, 0),
     
-    1 => array(BIOAUTH_FEATURE_DURATION, $lefthand, $lefthand, BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $righthand, $righthand, BIOAUTH_MEASURE_MEAN, 0),
+    2 => array(BIOAUTH_FEATURE_DURATION, $lefthand, $lefthand, BIOAUTH_MEASURE_MEAN, 0),
+    3 => array(BIOAUTH_FEATURE_DURATION, $righthand, $righthand, BIOAUTH_MEASURE_MEAN, 0),
     
-    1 => array(BIOAUTH_FEATURE_DURATION, $leftlittle, $leftlittle, BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $leftring, $leftring, BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $leftmiddle, $leftmiddle, BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $leftindex, $leftindex, BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $rightlittle, $rightlittle, BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $rightring, $rightring, BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $rightmiddle, $rightmiddle, BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $rightindex, $rightindex, BIOAUTH_MEASURE_MEAN, 0),
+    4 => array(BIOAUTH_FEATURE_DURATION, $leftlittle, $leftlittle, BIOAUTH_MEASURE_MEAN, 0),
+    5 => array(BIOAUTH_FEATURE_DURATION, $leftring, $leftring, BIOAUTH_MEASURE_MEAN, 0),
+    6 => array(BIOAUTH_FEATURE_DURATION, $leftmiddle, $leftmiddle, BIOAUTH_MEASURE_MEAN, 0),
+    7 => array(BIOAUTH_FEATURE_DURATION, $leftindex, $leftindex, BIOAUTH_MEASURE_MEAN, 0),
+    8 => array(BIOAUTH_FEATURE_DURATION, $rightlittle, $rightlittle, BIOAUTH_MEASURE_MEAN, 0),
+    9 => array(BIOAUTH_FEATURE_DURATION, $rightring, $rightring, BIOAUTH_MEASURE_MEAN, 0),
+    10 => array(BIOAUTH_FEATURE_DURATION, $rightmiddle, $rightmiddle, BIOAUTH_MEASURE_MEAN, 0),
+    11 => array(BIOAUTH_FEATURE_DURATION, $rightindex, $rightindex, BIOAUTH_MEASURE_MEAN, 0),
     
     // Left little
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('A'), $csvkeyids('A'), BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('Z'), $csvkeyids('Z'), BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('1'), $csvkeyids('1'), BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('Q'), $csvkeyids('Q'), BIOAUTH_MEASURE_MEAN, 0),
+    12 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('A'), $csvkeyids('A'), BIOAUTH_MEASURE_MEAN, 0),
+    13 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('Z'), $csvkeyids('Z'), BIOAUTH_MEASURE_MEAN, 0),
+    14 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('1'), $csvkeyids('1'), BIOAUTH_MEASURE_MEAN, 0),
+    15 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('Q'), $csvkeyids('Q'), BIOAUTH_MEASURE_MEAN, 0),
     // Left ring
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('S'), $csvkeyids('S'), BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('X'), $csvkeyids('X'), BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('2'), $csvkeyids('2'), BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('W'), $csvkeyids('W'), BIOAUTH_MEASURE_MEAN, 0),
+    16 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('S'), $csvkeyids('S'), BIOAUTH_MEASURE_MEAN, 0),
+    17 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('X'), $csvkeyids('X'), BIOAUTH_MEASURE_MEAN, 0),
+    18 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('2'), $csvkeyids('2'), BIOAUTH_MEASURE_MEAN, 0),
+    19 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('W'), $csvkeyids('W'), BIOAUTH_MEASURE_MEAN, 0),
     // Left middle
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('D'), $csvkeyids('D'), BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('C'), $csvkeyids('C'), BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('3'), $csvkeyids('3'), BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('E'), $csvkeyids('E'), BIOAUTH_MEASURE_MEAN, 0),
+    20 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('D'), $csvkeyids('D'), BIOAUTH_MEASURE_MEAN, 0),
+    21 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('C'), $csvkeyids('C'), BIOAUTH_MEASURE_MEAN, 0),
+    22 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('3'), $csvkeyids('3'), BIOAUTH_MEASURE_MEAN, 0),
+    23 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('E'), $csvkeyids('E'), BIOAUTH_MEASURE_MEAN, 0),
     // Left index
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('F'), $csvkeyids('F'), BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('B'), $csvkeyids('B'), BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('G'), $csvkeyids('G'), BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('R'), $csvkeyids('R'), BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('4'), $csvkeyids('4'), BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('T'), $csvkeyids('T'), BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('5'), $csvkeyids('5'), BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('V'), $csvkeyids('V'), BIOAUTH_MEASURE_MEAN, 0),
+    24 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('F'), $csvkeyids('F'), BIOAUTH_MEASURE_MEAN, 0),
+    25 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('B'), $csvkeyids('B'), BIOAUTH_MEASURE_MEAN, 0),
+    26 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('G'), $csvkeyids('G'), BIOAUTH_MEASURE_MEAN, 0),
+    27 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('R'), $csvkeyids('R'), BIOAUTH_MEASURE_MEAN, 0),
+    28 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('4'), $csvkeyids('4'), BIOAUTH_MEASURE_MEAN, 0),
+    29 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('T'), $csvkeyids('T'), BIOAUTH_MEASURE_MEAN, 0),
+    30 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('5'), $csvkeyids('5'), BIOAUTH_MEASURE_MEAN, 0),
+    31 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('V'), $csvkeyids('V'), BIOAUTH_MEASURE_MEAN, 0),
     // Right index
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('H'), $csvkeyids('H'), BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('M'), $csvkeyids('M'), BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('J'), $csvkeyids('J'), BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('Y'), $csvkeyids('Y'), BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('6'), $csvkeyids('6'), BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('U'), $csvkeyids('U'), BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('7'), $csvkeyids('7'), BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('N'), $csvkeyids('N'), BIOAUTH_MEASURE_MEAN, 0),
+    32 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('H'), $csvkeyids('H'), BIOAUTH_MEASURE_MEAN, 0),
+    33 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('M'), $csvkeyids('M'), BIOAUTH_MEASURE_MEAN, 0),
+    34 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('J'), $csvkeyids('J'), BIOAUTH_MEASURE_MEAN, 0),
+    35 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('Y'), $csvkeyids('Y'), BIOAUTH_MEASURE_MEAN, 0),
+    36 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('6'), $csvkeyids('6'), BIOAUTH_MEASURE_MEAN, 0),
+    37 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('U'), $csvkeyids('U'), BIOAUTH_MEASURE_MEAN, 0),
+    38 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('7'), $csvkeyids('7'), BIOAUTH_MEASURE_MEAN, 0),
+    39 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('N'), $csvkeyids('N'), BIOAUTH_MEASURE_MEAN, 0),
     // Right middle
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('K'), $csvkeyids('K'), BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('COMMA'), $csvkeyids('COMMA'), BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('8'), $csvkeyids('8'), BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('I'), $csvkeyids('I'), BIOAUTH_MEASURE_MEAN, 0),
+    40 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('K'), $csvkeyids('K'), BIOAUTH_MEASURE_MEAN, 0),
+    41 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('COMMA'), $csvkeyids('COMMA'), BIOAUTH_MEASURE_MEAN, 0),
+    42 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('8'), $csvkeyids('8'), BIOAUTH_MEASURE_MEAN, 0),
+    43 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('I'), $csvkeyids('I'), BIOAUTH_MEASURE_MEAN, 0),
     // Right ring
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('L'), $csvkeyids('L'), BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('PERIOD'), $csvkeyids('PERIOD'), BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('9'), $csvkeyids('9'), BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('O'), $csvkeyids('0'), BIOAUTH_MEASURE_MEAN, 0),
+    44 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('L'), $csvkeyids('L'), BIOAUTH_MEASURE_MEAN, 0),
+    45 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('PERIOD'), $csvkeyids('PERIOD'), BIOAUTH_MEASURE_MEAN, 0),
+    46 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('9'), $csvkeyids('9'), BIOAUTH_MEASURE_MEAN, 0),
+    47 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('O'), $csvkeyids('0'), BIOAUTH_MEASURE_MEAN, 0),
     // Right little
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('SEMICOLON'), $csvkeyids('SEMICOLON'), BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('SLASH'), $csvkeyids('SLASH'), BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('0'), $csvkeyids('0'), BIOAUTH_MEASURE_MEAN, 0),
-    1 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('P'), $csvkeyids('P'), BIOAUTH_MEASURE_MEAN, 0),
+    48 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('SEMICOLON'), $csvkeyids('SEMICOLON'), BIOAUTH_MEASURE_MEAN, 0),
+    49 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('SLASH'), $csvkeyids('SLASH'), BIOAUTH_MEASURE_MEAN, 0),
+    50 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('0'), $csvkeyids('0'), BIOAUTH_MEASURE_MEAN, 0),
+    51 => array(BIOAUTH_FEATURE_DURATION, $csvkeyids('P'), $csvkeyids('P'), BIOAUTH_MEASURE_MEAN, 0),
     
     /*
      * Mean type 1 transitions
      */
-    5 => array(BIOAUTH_FEATURE_T1, $letters, $letters, BIOAUTH_MEASURE_MEAN, 1),
-    5 => array(BIOAUTH_FEATURE_T1, $leftletters, $leftletters, BIOAUTH_MEASURE_MEAN, 1),
-    5 => array(BIOAUTH_FEATURE_T1, $rightletters, $rightletters, BIOAUTH_MEASURE_MEAN, 1),
-    5 => array(BIOAUTH_FEATURE_T1, $leftletters, $rightletters, BIOAUTH_MEASURE_MEAN, 1),
-    5 => array(BIOAUTH_FEATURE_T1, $rightletters, $leftletters, BIOAUTH_MEASURE_MEAN, 1),
+    52 => array(BIOAUTH_FEATURE_T1, $letters, $letters, BIOAUTH_MEASURE_MEAN, 1),
+    53 => array(BIOAUTH_FEATURE_T1, $leftletters, $leftletters, BIOAUTH_MEASURE_MEAN, 1),
+    54 => array(BIOAUTH_FEATURE_T1, $rightletters, $rightletters, BIOAUTH_MEASURE_MEAN, 1),
+    55 => array(BIOAUTH_FEATURE_T1, $leftletters, $rightletters, BIOAUTH_MEASURE_MEAN, 1),
+    56 => array(BIOAUTH_FEATURE_T1, $rightletters, $leftletters, BIOAUTH_MEASURE_MEAN, 1),
     // Left/Left
-    5 => array(BIOAUTH_FEATURE_T1, $csvkeyids('E'), $csvkeyids('R'), BIOAUTH_MEASURE_MEAN, 1),
-    5 => array(BIOAUTH_FEATURE_T1, $csvkeyids('A'), $csvkeyids('T'), BIOAUTH_MEASURE_MEAN, 1),
-    5 => array(BIOAUTH_FEATURE_T1, $csvkeyids('S'), $csvkeyids('T'), BIOAUTH_MEASURE_MEAN, 1),
-    5 => array(BIOAUTH_FEATURE_T1, $csvkeyids('R'), $csvkeyids('E'), BIOAUTH_MEASURE_MEAN, 1),
-    5 => array(BIOAUTH_FEATURE_T1, $csvkeyids('E'), $csvkeyids('S'), BIOAUTH_MEASURE_MEAN, 1),
-    5 => array(BIOAUTH_FEATURE_T1, $csvkeyids('E'), $csvkeyids('A'), BIOAUTH_MEASURE_MEAN, 1),
+    57 => array(BIOAUTH_FEATURE_T1, $csvkeyids('E'), $csvkeyids('R'), BIOAUTH_MEASURE_MEAN, 1),
+    58 => array(BIOAUTH_FEATURE_T1, $csvkeyids('A'), $csvkeyids('T'), BIOAUTH_MEASURE_MEAN, 1),
+    59 => array(BIOAUTH_FEATURE_T1, $csvkeyids('S'), $csvkeyids('T'), BIOAUTH_MEASURE_MEAN, 1),
+    60 => array(BIOAUTH_FEATURE_T1, $csvkeyids('R'), $csvkeyids('E'), BIOAUTH_MEASURE_MEAN, 1),
+    61 => array(BIOAUTH_FEATURE_T1, $csvkeyids('E'), $csvkeyids('S'), BIOAUTH_MEASURE_MEAN, 1),
+    62 => array(BIOAUTH_FEATURE_T1, $csvkeyids('E'), $csvkeyids('A'), BIOAUTH_MEASURE_MEAN, 1),
     // Right/Right
-    5 => array(BIOAUTH_FEATURE_T1, $csvkeyids('I'), $csvkeyids('N'), BIOAUTH_MEASURE_MEAN, 1),
-    5 => array(BIOAUTH_FEATURE_T1, $csvkeyids('O'), $csvkeyids('N'), BIOAUTH_MEASURE_MEAN, 1),
+    63 => array(BIOAUTH_FEATURE_T1, $csvkeyids('I'), $csvkeyids('N'), BIOAUTH_MEASURE_MEAN, 1),
+    64 => array(BIOAUTH_FEATURE_T1, $csvkeyids('O'), $csvkeyids('N'), BIOAUTH_MEASURE_MEAN, 1),
     // Left/Right
-    5 => array(BIOAUTH_FEATURE_T1, $csvkeyids('T'), $csvkeyids('H'), BIOAUTH_MEASURE_MEAN, 1),
-    5 => array(BIOAUTH_FEATURE_T1, $csvkeyids('E'), $csvkeyids('N'), BIOAUTH_MEASURE_MEAN, 1),
-    5 => array(BIOAUTH_FEATURE_T1, $csvkeyids('A'), $csvkeyids('N'), BIOAUTH_MEASURE_MEAN, 1),
-    5 => array(BIOAUTH_FEATURE_T1, $csvkeyids('T'), $csvkeyids('I'), BIOAUTH_MEASURE_MEAN, 1),
+    65 => array(BIOAUTH_FEATURE_T1, $csvkeyids('T'), $csvkeyids('H'), BIOAUTH_MEASURE_MEAN, 1),
+    66 => array(BIOAUTH_FEATURE_T1, $csvkeyids('E'), $csvkeyids('N'), BIOAUTH_MEASURE_MEAN, 1),
+    67 => array(BIOAUTH_FEATURE_T1, $csvkeyids('A'), $csvkeyids('N'), BIOAUTH_MEASURE_MEAN, 1),
+    68 => array(BIOAUTH_FEATURE_T1, $csvkeyids('T'), $csvkeyids('I'), BIOAUTH_MEASURE_MEAN, 1),
     // Right/Left
-    5 => array(BIOAUTH_FEATURE_T1, $csvkeyids('N'), $csvkeyids('D'), BIOAUTH_MEASURE_MEAN, 1),
-    5 => array(BIOAUTH_FEATURE_T1, $csvkeyids('H'), $csvkeyids('E'), BIOAUTH_MEASURE_MEAN, 1),
-    5 => array(BIOAUTH_FEATURE_T1, $csvkeyids('O'), $csvkeyids('R'), BIOAUTH_MEASURE_MEAN, 1),
+    69 => array(BIOAUTH_FEATURE_T1, $csvkeyids('N'), $csvkeyids('D'), BIOAUTH_MEASURE_MEAN, 1),
+    70 => array(BIOAUTH_FEATURE_T1, $csvkeyids('H'), $csvkeyids('E'), BIOAUTH_MEASURE_MEAN, 1),
+    71 => array(BIOAUTH_FEATURE_T1, $csvkeyids('O'), $csvkeyids('R'), BIOAUTH_MEASURE_MEAN, 1),
     
     );
 

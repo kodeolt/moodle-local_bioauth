c702141ae3ec160d5c57995c50e6ac813f722204
Vinnie Monaco <vinmonaco@gmail.com>
2014-05-30 11:31:32 -0400

    Added user and choice download scripts

---
+++ biodata/choices.php
@@ -0,0 +1,69 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Main page for the BioAuth reporting capabilities.
+ * This gives an overview of courses
+ * 
+ * @package    local_bioauth
+ * @copyright  Vinnie Monaco
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once(dirname(__FILE__) . '/../../../config.php');
+require_once($CFG->dirroot . '/local/bioauth/locallib.php');
+
+$biometric = required_param('biometric', PARAM_TEXT);
+
+$PAGE->set_url(new moodle_url('/local/bioauth/biodata/choices.php'));
+
+require_login();
+
+global $DB;
+
+$choices = $DB->get_records_sql(
+'SELECT u.id,u.email,u.username,c.name,o.text
+FROM mdl_user u, mdl_choice_answers a, mdl_choice_options o, mdl_choice c
+WHERE a.userid = u.id
+AND a.choiceid = c.id
+AND a.optionid = o.id');
+
+if (count($choices) === 0) {
+    echo 'No data to download';
+    die();
+}
+
+$filename = 'choices.csv';
+$columns = 'userid,email,username,choice,answer';
+
+header("Content-type: text/csv");
+header("Content-Disposition: attachment; filename=$filename.csv");
+header("Pragma: no-cache");
+header("Expires: 0");
+$output = fopen('php://output', 'w');
+
+fputs($output, $fixed_columns . "\n");
+
+foreach ($users as $idx => $choice) {
+    $fields = array();
+    $fields[] = $choice->userid;
+    $fields[] = $choice->email;
+    $fields[] = $choice->username;
+    $fields[] = $choice->name;
+    $fields[] = $choice->text;
+    $fields_str = csv_str($fields);
+    fputs($output, $fields_str . "," . "\n");
+}
---
+++ biodata/users.php
@@ -0,0 +1,62 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Main page for the BioAuth reporting capabilities.
+ * This gives an overview of courses
+ * 
+ * @package    local_bioauth
+ * @copyright  Vinnie Monaco
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once(dirname(__FILE__) . '/../../../config.php');
+require_once($CFG->dirroot . '/local/bioauth/locallib.php');
+
+$biometric = required_param('biometric', PARAM_TEXT);
+
+$PAGE->set_url(new moodle_url('/local/bioauth/biodata/users.php'));
+
+require_login();
+
+global $DB;
+
+$users = $DB->get_records_sql('SELECT u.id,u.email,u.username FROM mdl_user u');
+
+if (count($biodata) === 0) {
+    echo 'No data to download';
+    die();
+}
+
+$filename = 'users.csv';
+$columns = 'userid,email,username';
+
+header("Content-type: text/csv");
+header("Content-Disposition: attachment; filename=$filename.csv");
+header("Pragma: no-cache");
+header("Expires: 0");
+$output = fopen('php://output', 'w');
+
+fputs($output, $fixed_columns . "\n");
+
+foreach ($users as $idx => $user) {
+    $fields = array();
+    $fields[] = $user->id;
+    $fields[] = $user->email;
+    $fields[] = $user->username;
+    $fields_str = csv_str($fields);
+    fputs($output, $fields_str . "," . "\n");
+}
